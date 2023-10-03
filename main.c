66af8479a5b678dc8cf6a7d5397d208f01926ea6
Ayoub GTC <ayoubsnini@gmail.com>
2023-10-03 11:31:43 +0100

    seconde

--- main.c
+++ main.c
@@ -8,20 +8,7 @@
 int main()
 {
 
-    int tmp;
-    int taille=10;
-    int T[] ={3,2,1,8,0,7,9,6,2};
-    int i,j;
-    for (i=0;i<taille-1;i++){
-        for (j=i+1;i<taille;j++){
-            if(T[i]<T[j])
-                tmp = T[i];
-               T[i]= T[j];
-               T[j]= tmp;
-        }
-    }
-    for (i=0;i<taille;i++)
-    printf("%d ",T[i]);
+    printf("hello world!!");
 
 
 
